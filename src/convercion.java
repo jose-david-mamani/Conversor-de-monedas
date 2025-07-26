50f4b23caebfffa1f3d39d08c920b3c685fe456c
El_KRAX <josemamanila51@gmail.com>
2025-07-25 22:58:42 -0300

    se eliminaron las varias classes que no cumplian ninguna funcion en el codigo y se empezo con el codigo muy primitivo

---
+++ src/convercion.java
@@ -0,0 +1,11 @@
+public class convercion {
+    public static void main(String[] args) {
+
+        String monedas = """
+                aqui van las distintas minedas
+                """;
+        System.out.println("que moneda desea utilizar como moneda base");
+
+        System.out.println(monedas);
+    }
+}
